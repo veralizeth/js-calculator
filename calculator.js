fa011d2dddd54c92fc0e0e3154dd82efdf3809a0
Vera <lizeth.veraro@outlook.com>
2020-03-31 21:06:37 -0700

    Refactoring the code to create the objects dinamicly 'I assume that is dinamicly'

--- calculator.js
+++ calculator.js
@@ -1,5 +1,53 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
+
+function number_validation(number) {
+  if (isNaN(number)) {
+    return false;
+  }
+  else if (number == null || number === ' ') {
+    return false;
+  }
+  else {
+    return Number(number)
+  }
 }
+
+function Operation(num1, num2, operation) {
+  this.num1 = num1;
+  this.num2 = num2;
+  this.operation = operation;
+}
+
+const add = new Operation(1,2,['add','+']);
+const sub = new Operation(1,2,['substract','-']);
+const multiply = new Operation(1,2,['multiply','*']);
+const divide = new Operation(1,2,['divide','/']);
+
+function calculate(input) {
+  // Input !== as a condition because JS takes zero as a falsy value.s
+  if(!number_validation(input.num1) && number_validation(input.num1) !== 0){
+    return input.num1 + ' is not a valid number. Please enter a valid number';
+  }
+  if(!number_validation(input.num2) && number_validation(input.num2) !== 0){
+    return input.num2 + ' is not a valid number. Please enter a valid number';
+  }
+  if (input.operation.includes('add') || input.operation.includes('+')) {
+    return input.num1 + input.num2
+  }
+  if (input.operation.includes('subtract') || input.operation.includes('-')) {
+    return input.num1 - input.num2
+  } 
+  if (input.operation.includes('multiply') || input.operation.includes('*')) {
+    return input.num1 * input.num2
+  }
+  if (input.operation.includes('divide') || input.operation.includes('/')) {
+    return input.num1 / input.num2
+  } 
+  else {
+    return 'Please enter a valid operator.'
+  }
+}
+
+console.log(calculate(add));
+console.log(calculate(sub));
+console.log(calculate(multiply));
+console.log(calculate(divide));
